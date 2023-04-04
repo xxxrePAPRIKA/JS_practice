c72e1c212bc4a478b8c834128ce1e2e546557824
xxxrePAPRIKA <xxxre.paprika@gmail.com>
2023-04-04 15:37:37 +0700

    class at js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 'use strict';
 // const btns = document.querySelectorAll('button');
 // const wrapper = document.querySelector('.btn-block')
 
@@ -23,32 +24,32 @@
 // btn.classList.add('red');
 // wrapper.append(btn)
 
-let btn = document.querySelector('.btn'),
-    timerId,
-    i = 0;
+// let btn = document.querySelector('.btn'),
+//     timerId,
+//     i = 0;
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+// function myAnimation() {
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
 
-    const id = setInterval(frame, 10)
-    function frame() {
-        if(pos == 300) {
-            clearInterval(id)
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+//     const id = setInterval(frame, 10)
+//     function frame() {
+//         if(pos == 300) {
+//             clearInterval(id)
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+//         }
+//     }
     
-}
+// }
 
-btn.addEventListener('click', () => {
-    // const timerId = setTimeout(logger, 2000);
-    // timerId = setInterval(logger, 2000);
-    myAnimation();
-})
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000);
+//     // timerId = setInterval(logger, 2000);
+//     myAnimation();
+// })
 
 // clearInterval(timerId);
 
@@ -93,3 +94,166 @@ btn.addEventListener('click', () => {
 // }
 
 // window.requestAnimationFrame(moveBall);
+
+// function outer() {
+//     const potentiallyHugeArray = [];
+//     return function inner() {
+//         potentiallyHugeArray.push('hello');
+//         console.log('hello');
+//     }
+// }
+
+// const sayHello = outer();
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Работа с датами
+'use strict';
+
+// const now = new Date();
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCDate());
+
+// console.log(now.getTimezoneOffset());
+
+
+
+let start = new Date();
+
+for(let i = 0; i < 10000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл for отработал за ${end - start} милисекунд`)
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// const box = document.querySelector('.box');
+
+// let observer = new MutationObserver(mutationRecords => {
+//     console.log(mutationRecords);
+// });
+
+// observer.observe(box, {
+//     childList: true
+// });
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`hello ${this.name}`); 
+//     }
+// }
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20)
+
+// ivan.hello();
+// alex.hello();
+
+// User.prototype.exit = function() {
+//     console.log(`User ${this.name} leave`);
+// };
+
+// ivan.exit();
+
+
+///////////////////////////////////////////////////////////////////////////
+
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//     console.log(this);
+//         return a + b
+//     }
+
+//     console.log(sum());
+// };
+
+// showThis(4, 5);
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// }
+
+// obj.sum()
+
+// function sayName() {
+//     console.log(this);
+//     console.log(this.name);
+// };
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user);
+// sayName.apply(user);
+
+// function count(num) {
+//     return this*num;
+// };
+
+// const double = count.bind(2);
+// console.log(double(3));
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+//     console.log(this);
+// });
+
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+};
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`текс: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+
+
+const div = new ColoredRectangleWithText(25, 10, 'Helloworld', 'red')
+div.showMyProps();
+console.log(div.calcArea());
+
+
+
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(square.calcArea());
+console.log(square);
+console.log(long.calcArea());
+console.log(long);
